8f322cb6fa36eb440d2095cd3c3fb4d5b6346940
MwambiaBrian <brianmwambia4@gmail.com>
2024-01-25 12:44:21 +0300

    img

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -6,11 +6,11 @@ function ListingItem({ listing }) {
   return (
     <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
       <Link to={`/listing/${listing._id}`}>
-        <img
+        {/* <img
           src={listing.imageUrls[0]}
           alt="listing cover"
           className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
-        />
+        /> */}
         <div className="p-3 flex flex-col gap-2 w-full">
           <p className="truncate text-lg font-semibold text-slate-700">
             {listing.name}
@@ -46,5 +46,4 @@ function ListingItem({ listing }) {
     </div>
   );
 }
-
 export default ListingItem;
