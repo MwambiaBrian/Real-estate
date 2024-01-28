9f3d7d70ebd01fd65b37238baef8a52e7fe92410
MwambiaBrian <brianmwambia4@gmail.com>
2024-01-28 11:16:46 +0300

    listing

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -57,19 +57,17 @@ function Listing() {
       {listing && !error && !loading && (
         <div>
           <Swiper navigation>
-            {listing.imageUrls &&
-              listing.imageUrls.length > 0 &&
-              listing.imageUrls.map((url) => (
-                <SwiperSlide key={url}>
-                  <div
-                    className="h-[550px]"
-                    style={{
-                      background: `url(${url}) center no-repeat`,
-                      backgroundSize: "cover",
-                    }}
-                  ></div>
-                </SwiperSlide>
-              ))}
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
           </Swiper>
           <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
             <FaShare
